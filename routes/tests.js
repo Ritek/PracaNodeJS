33e62a93dfc4ef68be7948c219571208624238f2
Wojciech <writhaler@gmail.com>
2019-10-09 22:18:03 +0200

    added delete test route

--- routes/tests.js
+++ routes/tests.js
@@ -38,7 +38,7 @@ router.post('/gettests', async (req, res) => {
                 for (let i=0;i<result.length;i++) {
                     tempArr.push({id: result[i]._id, name: result[i].name, tags: result[i].tags, exnum: result[i].questions.length});
                 }
-                console.log(tempArr);
+                //console.log(tempArr);
                 res.status(200).send(tempArr);
             }
         });
@@ -70,13 +70,44 @@ router.post('/gettest', async (req, res) => {
 
 
 router.post('/edittest', async (req, res) => {
-    let teacherId = oID(req.body.id);
     let test = req.body.test;
+    let teacherId = oID(req.body.id);
+    let testId = oID(test._id);
 
-    console.log('Edit');
-    console.log('test:', test);
+    //console.log(teacherId);
+    //console.log(testId);
 
-    res.status(200).send('ok');
+    try {
+        var dataBase = db.getDb();
+
+        await dataBase.collection('tests').updateOne({_id: testId, author: teacherId}, 
+            {$set: {name: test.name, tags: test.tags, questions: test.questions}
+        });
+        res.send('ok');
+    } catch(error) {
+        console.log(error);
+    }
+
+});
+
+
+router.post('/deletetest', async (req, res) => {
+    let testId = oID(req.body.testId);
+    let teacherId = oID(req.body.userId);
+
+    try {
+        var dataBase = db.getDb();
+        await dataBase.collection('tests').deleteOne({_id: testId})
+        .then(res => {
+            res.send('deleted');
+        }).catch(error => {
+            console.log(error);
+            res.status(400).send('error');
+        })
+
+    } catch(error) {
+        console.log(error);
+    }
 
 });
 
